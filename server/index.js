f28018a8a15d34ff17f26eba1e9398d8bdddce02
Richard Durazo <rcdurazo@gmail.com>
2017-05-05 20:09:48 -0700

    add user model and auth controller connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
+const router = require('./router')
+const mongoose = require('mongoose')
+
+//db setup
+mongoose.connect('mongodb://localhost:auth/auth');
 // app setup
+// any incomming request from our server will be passed into morgan and body paser 
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 //server setup
 const port = process.env.port || 3090;
