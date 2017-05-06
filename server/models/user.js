c201a6a31e60e99aedcfcd491120e40eea32d1dd
Richard Durazo <rcdurazo@gmail.com>
2017-05-05 20:22:25 -0700

    add bcrypt to has user pw and save hashed value to db

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
-
+//define model
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true }, 
     password: String
 });
 
+//on save hook, encrypt password
+// before saving a mode, run this function
+userSchema.pre('save', function(next) {
+    // get access to the user model 
+    const user = this; // user.email or user.password
 
+    // generate a salt then run callback 
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) { return next(err); }
+        // hash (encrypt) the pw using salt
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) {return next(err); }
+            // overwrite plain text plassword with hash
+            user.password = hash;
+            next();
+        });
+    });
+});
+//create model class
 const ModelClass = mongoose.model('user', userSchema);
 
 module.exports = ModelClass;
